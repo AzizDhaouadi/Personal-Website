bfc6ee8a9981acb0e95effdb3fc1cf59ec23b40b
Aziz Dhaouadi <dhaziz.works@gmail.com>
2024-10-25 23:18:48 -0400

    Added a socialLinks component

---
+++ src/components/socialLinks.tsx
@@ -0,0 +1,34 @@
+import { List, ListItem, Link } from "@chakra-ui/react";
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { ImBlog } from "react-icons/im";
+
+
+function SocialLinks() {
+    return (
+        <List spacing={"3"} textAlign={"left"}>
+            <ListItem>
+                <Link href="https://datajournal.datakyu.co/" fontSize={"2xl"} isExternal>
+                    <ExternalLinkIcon boxSize={6} as={ImBlog} mx={"1rem"} />
+                    Blog
+                </Link>
+            </ListItem>
+            <ListItem>
+                <Link href="https://www.linkedin.com/in/aziz-dhaouadi/" fontSize={"2xl"} isExternal>
+                    <ExternalLinkIcon boxSize={6} as={FaLinkedin} mx={"1rem"} />
+                    LinkedIn
+                </Link>
+            </ListItem>
+            <ListItem>
+                <Link href="https://github.com/AzizDhaouadi" fontSize={"2xl"} isExternal>
+                    <ExternalLinkIcon boxSize={6} as={FaGithub} mx={"1rem"} />
+                    Github
+                </Link>
+            </ListItem>
+        </List>
+    )
+}
+
+
+export default SocialLinks;
